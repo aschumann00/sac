18fa8068354e006af7fe8682c9f2a3bac9782c12
aschumann00 <103746235+aschumann00@users.noreply.github.com>
2022-04-16 08:29:05 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,3 +1,4 @@
+// MessageToast
 (function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
@@ -41,37 +42,42 @@
 		onCustomWidgetAfterUpdate(oChangedProperties) {
             if (this._firstConnection){
 
-/* 				let inputFile =	document.createElement("input");
-				inputFile.setAttribute("id","readLocalFile");
-				inputFile.setAttribute("type","file");
-				inputFile.setAttribute("accept","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel");
-				inputFile.setAttribute("multiple", false);  //Multiple choices are allowed. Set to no here
-				inputFile.click();
-				
-				document.body.appendChild(inputFile);
-					console.log("Use ID by readLocalFile Of input dom Object to get the contents of the uploaded file json Format data return");
-					document.getElementById('readLocalFile').addEventListener('change', function(e) {   //Execute after selecting file
-						let  files = e.target.files;
-						if(files.length == 0) return;
-						let  file = files[0];
-						let  reader = new FileReader();
-						reader.readAsBinaryString(file);
-						reader.onload = function(e) {   //Handle the load event. Triggered when the read operation completes.
-							let  data = e.target.result;
-							let  workbook = XLSX.read(data, {type: 'binary'}); //XLSX: / xlsx.core.min.js read excel through XLSX.read(data, {type: type}) method
-							console.log(workbook ); 
-							let  sheetNames = workbook.SheetNames; // Sheet name collection
-							let  worksheet = workbook.Sheets[sheetNames[0]]; // Here we only read the first sheet 
-							let  json = XLSX.utils.sheet_to_json(worksheet); //  Read the workbook, you can write your own methods to output the table. Here it is recommended to use XLSX.utils. Tool class to output the data. Here you can output the data in json format and other format codes
-							if(typeof(callback) == "function") callback(json);   //Callback 
-							 document.getElementById('readLocalFile').value = null; //Clear after reading
-						};
-					}); */
+				var fU = this.getView().byId("idfileUploader");
+				var domRef = fU.getFocusDomRef();
+				var file = domRef.files[0];
+				var this_ = this;
 
-                MessageToast.show("Maximum records are 2000.");
+				var oModel = new JSONModel();
+				oModel.setData({
+				result_final: null
+				});
+
+				var reader = new FileReader();
+				reader.onload = async function(e) {
+				var strCSV = e.target.result;
+
+				var workbook = XLSX.read(strCSV, {
+				  type: 'binary'
+				});
+
+				var result_final = [];
+				var result = [];
+				var correctsheet = false;
+
+				workbook.SheetNames.forEach(function(sheetName) {
+				  if (sheetName === "Sheet1") {
+					correctsheet = true;
+					var csv = XLSX.utils.sheet_to_csv(workbook.Sheets[sheetName]);
+					if (csv.length) {
+					  result.push(csv);
+					}
+					result = result.join("[$@~!~@$]")
+				  }
+				});
+
 				console.log(oChangedProperties['value']);
 				alert("Alert");
-                this.redraw();
+				this.redraw();
             }
         }
         
