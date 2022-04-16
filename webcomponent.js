bc75e37d995c3be6375992207b5e868f6ccad482
aschumann00 <103746235+aschumann00@users.noreply.github.com>
2022-04-16 10:23:59 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -8,11 +8,6 @@
 		<br>
         <div id="headline"><b>Hello World</b></div>
 		<br><br>
-		<input type="file" id="fileUpload" />
-		<input type="button" id="upload" value="Upload" onclick="UploadProcess()" />
-		<br/>
-
-		<div id="ExcelTable"></div>		
 		</body>
 	</html>
     `;
@@ -63,39 +58,12 @@
 
 
 		uploadProcess() {		
-		var fileUpload = document.getElementById("fileUpload");
- 
-        //Validate whether File is valid Excel file.
-        var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xls|.xlsx)$/;
-        if (regex.test(fileUpload.value.toLowerCase())) {
-            if (typeof (FileReader) != "undefined") {
-                var reader = new FileReader();
- 
-                //For Browsers other than IE.
-                if (reader.readAsBinaryString) {
-                    reader.onload = function (e) {
-                        GetTableFromExcel(e.target.result);
-                    };
-                    reader.readAsBinaryString(fileUpload.files[0]);
-                } else {
-                    //For IE Browser.
-                    reader.onload = function (e) {
-                        var data = "";
-                        var bytes = new Uint8Array(e.target.result);
-                        for (var i = 0; i < bytes.byteLength; i++) {
-                            data += String.fromCharCode(bytes[i]);
-                        }
-                        GetTableFromExcel(data);
-                    };
-                    reader.readAsArrayBuffer(fileUpload.files[0]);
-                }
-            } else {
-                alert("This browser does not support HTML5.");
-            }
-        } else {
-            alert("Please upload a valid Excel file.");
-        }
-		};
+			var inputElement = document.getElementById("input");
+inputElement.addEventListener("change", handleFiles, false);
+function handleFiles() {
+  var fileList = this.files; /* Jetzt kann die Dateiliste verarbeitet werden */
+}
+		}
 	
 	
         
