4758ccb8d2e907f09c225bf1296c03039219ebaf
aschumann00 <103746235+aschumann00@users.noreply.github.com>
2022-04-20 10:58:31 +0200

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -62,15 +62,7 @@
 		uploadProcess() {	
 			console.log('Methode uploadProcess');
 		
-			import readXlsxFile from 'read-excel-file'
-			const input = document.getElementById('input')
-
-			input.addEventListener('change', () => {
-			  readXlsxFile(input.files[0]).then((data) => {
-				// `data` is an array of rows
-				// each row being an array of cells.
-			  })
-			})
+			
 		}
 	
 	
